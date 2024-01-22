6400f973ba6dcafbdb8996341db27ca170fbd90a
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-01-22 14:25:45 +0530

    Service namespace changed

--- FirstStep/Models/DTOs/AdvertisementDTO.cs
+++ FirstStep/Models/DTOs/AdvertisementDTO.cs
@@ -2,17 +2,5 @@
 {
     public class AdvertisementDto
     {
-        public required string advertisement_title { get; set; }
-        public required string advertisement_description { get; set; }
-        public required string advertisement_location { get; set; }
-        public required string advertisement_salary { get; set; }
-        public required string advertisement_contact { get; set; }
-        public required string advertisement_email { get; set; }
-        public required string advertisement_website { get; set; }
-        public required string advertisement_image { get; set; }
-        public required string advertisement_video { get; set; }
-        public required string advertisement_date { get; set; }
-        public required List<string> professionKeywords { get; set; }
-        public required string field_name { get; set; }
     }
 }
