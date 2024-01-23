0e8d22d678c07248f80013bf260cf67e72aa4357
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-01-23 09:43:36 +0530

    Add company service and controller

---
+++ FirstStep/Services/CompanyServices/ICompanyService.cs
@@ -0,0 +1,17 @@
+﻿using FirstStep.Models;
+
+namespace FirstStep.Services
+{
+    public interface ICompanyService
+    {
+        public Task<IEnumerable<Company>> GetAll();
+
+        public Task<Company> GetById(int id);
+
+        public Task<Company> Create(Company company);
+
+        public void Update(Company company);
+
+        public void Delete(int id);
+    }
+}
