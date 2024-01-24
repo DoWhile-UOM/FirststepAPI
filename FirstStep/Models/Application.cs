6e4e3c932dcfd9e9c5fd4446f7f335399bebfccc
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-01-24 16:39:55 +0530

    Add application class

--- FirstStep/Models/Application.cs
+++ FirstStep/Models/Application.cs
@@ -1,6 +1,31 @@
-﻿namespace FirstStep.Models
-{
-    public class Application
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstStep.Models
     {
+        public class Application
+        {
+            [Key]
+            public required string applicationId { get; set; }
+
+            public required string firstName { get; set; }
+
+            public required string lastName { get; set; }
+
+            public required string email { get; set; }
+
+            public required string phoneNumber { get; set; }
+
+            public required bool status { get; set; }
+
+            public DateTime? lastReviewDate { get; set; }
+
+            public string? lastComment { get; set; }
+
+            public string? linkedinURL { get; set; }
+
+            public required DateTime submittedDate { get; set; }
+        }
     }
-}
+
+
+
