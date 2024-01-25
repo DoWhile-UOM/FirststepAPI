a97a6554e2dd08ed875bf7886edccfaac1dfba51
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-01-25 15:20:28 +0530

    Change HR Manager controller as Employee controller

---
+++ FirstStep/Services/EmployeeServices/EmployeeService.cs
@@ -0,0 +1,66 @@
+﻿using FirstStep.Data;
+using FirstStep.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstStep.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly DataContext _context;
+
+        public EmployeeService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Employee> Create(HRManager employee)
+        {
+            employee.user_id = 0;
+
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return employee;
+        }
+
+        public async void Delete(int id)
+        {
+            Employee employee = await GetById(id);
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetById(int id)
+        {
+            Employee? employee = await _context.Employees.FindAsync(id);
+            if (employee is null)
+            {
+                throw new Exception("Employee not found.");
+            }
+
+            return employee;
+        }
+
+
+        public async void Update(Employee employee)
+        {
+            Employee dbEmployee = await GetById(employee.user_id);
+
+            dbEmployee.first_name = employee.first_name;
+            dbEmployee.last_name = employee.last_name;
+            dbEmployee.email = employee.email;
+            dbEmployee.password = employee.password;
+            dbEmployee.user_id = employee.user_id;
+            dbEmployee.company_id = employee.company_id;
+
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ FirstStep/Services/EmployeeServices/IEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using FirstStep.Models;
+
+namespace FirstStep.Services
+{
+    public interface IEmployeeService
+    {
+        public Task<IEnumerable<Employee>> GetAll();
+
+        public Task<Employee> GetById(int id);
+
+        public Task<Employee> Create(HRManager employee);
+
+        //void Update(int id, Employee item);
+        public void Update(Employee employee);
+
+        public void Delete(int id);
+    }
+}
