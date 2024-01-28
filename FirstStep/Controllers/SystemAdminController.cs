2139dd7dad3700f1c2ccf6c4def53ada1f4fd3b4
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-01-28 16:39:16 +0530

    completed system admin contoller.

--- FirstStep/Controllers/SystemAdminController.cs
+++ FirstStep/Controllers/SystemAdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FirstStep.Models;
+using FirstStep.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FirstStep.Controllers
@@ -7,5 +9,48 @@ namespace FirstStep.Controllers
     [ApiController]
     public class SystemAdminController : ControllerBase
     {
+        private readonly ISystemAdminService _service;
+
+        [HttpGet]
+        [Route("GetAllSystemAdmins")]
+
+        public async Task<ActionResult<IEnumerable<SystemAdmin>>> GetAllSystemAdmins()
+        {
+            return Ok(await _service.GetAll());
+        }
+
+        [HttpGet]
+        [Route("GetSystemAdminById/{id}")]
+
+        public async Task<ActionResult<SystemAdmin>> GetSystemAdminById(int id)
+        {
+            return Ok(await _service.GetById(id));
+        }
+
+        [HttpPost]
+        [Route("AddSystemAdmin")]
+
+        public async Task<IActionResult> AddSystemAdmin(SystemAdmin systemAdmin)
+        {
+            await _service.Create(systemAdmin);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("UpdateSystemAdmin")]
+
+        public async Task<IActionResult> UpdateSystemAdmin(SystemAdmin reqsystemAdmin)
+        {
+            await _service.Update(reqsystemAdmin);
+            return Ok($"Successfully Updated Application ID: {reqsystemAdmin.user_id}");
+        }
+        [HttpDelete]
+        [Route("DeleteSystemAdmin/{id}")]
+
+        public async Task<IActionResult> DeleteSystemAdmin(int id)
+        {
+            await _service.Delete(id);
+            return Ok();
+        }
     }
 }
