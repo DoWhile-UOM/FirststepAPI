dd31c3e9494e03f73d60b70b0d1fe15419909b2a
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-01-28 16:10:48 +0530

    SystemAdmin service was completed

---
+++ FirstStep/Migrations/20240126103322_SystemAdimClasswasChanged.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstStep.Migrations
+{
+    /// <inheritdoc />
+    public partial class SystemAdimClasswasChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SystemAdmis_Users_user_id",
+                table: "SystemAdmis");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SystemAdmis",
+                table: "SystemAdmis");
+
+            migrationBuilder.RenameTable(
+                name: "SystemAdmis",
+                newName: "SystemAdmins");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SystemAdmins",
+                table: "SystemAdmins",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SystemAdmins_Users_user_id",
+                table: "SystemAdmins",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SystemAdmins_Users_user_id",
+                table: "SystemAdmins");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SystemAdmins",
+                table: "SystemAdmins");
+
+            migrationBuilder.RenameTable(
+                name: "SystemAdmins",
+                newName: "SystemAdmis");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SystemAdmis",
+                table: "SystemAdmis",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SystemAdmis_Users_user_id",
+                table: "SystemAdmis",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ FirstStep/Services/SystemAdminServices/ISystemAdminService.cs
@@ -0,0 +1,17 @@
+﻿using FirstStep.Models;
+
+namespace FirstStep.Services.SystemAdminServices
+{
+    public interface ISystemAdminService
+    {
+        Task<IEnumerable<SystemAdmin>> GetAll();
+
+        Task<SystemAdmin> GetById(int id);
+
+        Task Create(SystemAdmin systemAdmin);
+
+        Task Update(SystemAdmin systemAdmin);
+
+        Task Delete(int id);
+    }
+}
