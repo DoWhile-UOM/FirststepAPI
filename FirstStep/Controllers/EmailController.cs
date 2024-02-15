9b16bc2fdcae6ea84a043d134263eec5653be70e
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-02-15 18:22:03 +0530

    add email controller

---
+++ FirstStep/Controllers/EmailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstStep.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstStep/Models/DTOs/RequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstStep.Models.DTOs
+{
+    public class RequestDTO
+    {
+        public string To { get; set; } = string.Empty;
+        public string ClientName { get; set; } = string.Empty;
+        public string Deta { get; set; } = string.Empty;
+        public string LicenseKey { get; set; } = string.Empty;
+
+    }
+}
