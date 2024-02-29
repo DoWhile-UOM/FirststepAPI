05e35ae688fe496dfe037229648a5a1cc4050bc3
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-03-01 02:19:46 +0530

    changed email controller

--- FirstStep/Controllers/EmailController.cs
+++ FirstStep/Controllers/EmailController.cs
@@ -6,22 +6,51 @@ using MimeKit.Text;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using FirstStep.Services.EmailSevices;
+using Grpc.Core;
 
 // this controller is built to have a breif idea on how to use the esnd email service
 // this controller is more like a temp sample controller
 
 namespace FirstStep.Controllers
 {
-    public class EmailController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
     {
         private readonly IEmailService _emailService;
         public EmailController(IEmailService emailService) {
             _emailService = emailService;
         }
         [HttpPost]
-        public IActionResult SendEmail(EmailDto request)
+        [Route("SendEmail")]
+        public IActionResult SendEmail(String email, int type)
         {
-            _emailService.SendEmail(request);
+            if (type == 0)
+            {
+                // OTP Email
+                EmailDto request = new();
+                var builder = new BodyBuilder();
+                Random random = new Random();
+                int otp = random.Next(100000, 999999);
+                using (StreamReader SourceReader = System.IO.File.OpenText("~/Template/EmailTemplate.htm"))
+                {
+                    builder.HtmlBody = SourceReader.ReadToEnd();
+                } 
+               
+                
+                request.To = email;
+                request.Subject = "FirstStep Verification OTP";
+                builder.HtmlBody = builder.HtmlBody.Replace("{OTP}", otp.ToString());
+                request.Body = builder.HtmlBody;
+               // request.Body = "<p>Use this as the verification otp</p><br><h4>OTP :" + otp.ToString() + "</h4>";
+               
+                _emailService.SendEmail(request);
+            }
+            else if (type == 1)
+            {
+                // Registration Email
+            }
+            
 
             return Ok();
         }
