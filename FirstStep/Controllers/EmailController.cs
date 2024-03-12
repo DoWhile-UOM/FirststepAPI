2e200e558ab5debd183aa0b8c31801d4f02c870f
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-03-13 03:52:21 +0530

    templates for otp and registration succesfull was created

--- FirstStep/Controllers/EmailController.cs
+++ FirstStep/Controllers/EmailController.cs
@@ -7,6 +7,8 @@ using MailKit.Net.Smtp;
 using MailKit.Security;
 using FirstStep.Services.EmailSevices;
 using Grpc.Core;
+using System.IO;
+using static System.Net.WebRequestMethods;
 
 // this controller is built to have a breif idea on how to use the esnd email service
 // this controller is more like a temp sample controller
@@ -32,26 +34,42 @@ namespace FirstStep.Controllers
                 var builder = new BodyBuilder();
                 Random random = new Random();
                 int otp = random.Next(100000, 999999);
-               using (StreamReader SourceReader = System.IO.File.OpenText("~/Template/EmailTemplate.html"))
+               using (StreamReader SourceReader = System.IO.File.OpenText("Template/OTPEmailService.html"))
                 {
                     builder.HtmlBody = SourceReader.ReadToEnd();
-                } 
-               
-                
+                }
+
+
+
                 request.To = email;
                 request.Subject = "FirstStep Verification OTP";
                 builder.HtmlBody = builder.HtmlBody.Replace("{OTP}", otp.ToString());
                 request.Body = builder.HtmlBody;
 
-               // request.Body = "<p>Use this as the verification otp</p><br><h4>OTP :" + otp.ToString() + "</h4>";
+       
                
                 _emailService.SendEmail(request);
             }
             else if (type == 1)
             {
                 // Registration Email
+                //
+                EmailDto request = new();
+                var builder = new BodyBuilder();
+                using (StreamReader SourceReader = System.IO.File.OpenText("Template/ApplicationSentEmail.html"))
+                {
+                    builder.HtmlBody = SourceReader.ReadToEnd();
+                }
+                request.To = email;
+                request.Subject = "Application was successfully sent";
+                builder.HtmlBody = builder.HtmlBody.Replace("{username}", email);
+                request.Body = builder.HtmlBody;
+
+
+
+                _emailService.SendEmail(request);
             }
-            
+
 
             return Ok();
         }
