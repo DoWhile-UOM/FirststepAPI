7f966ee72d2c7bb7a07836a0976b441002a3a36d
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-03-13 22:52:29 +0530

    seeker

--- FirstStep/Models/DTOs/SeekerApplicationDto.cs
+++ FirstStep/Models/DTOs/SeekerApplicationDto.cs
@@ -2,5 +2,17 @@
 {
     public class SeekerApplicationDto
     {
+        public required string email { get; set; }
+
+        public required string first_name { get; set; }
+
+        public required string last_name { get; set; }
+
+        public int phone_number { get; set; }
+
+        public string? linkedin { get; set; }
+
+        public string? Cv { get; set; }
+
     }
 }
