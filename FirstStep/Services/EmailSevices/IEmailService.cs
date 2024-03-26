a1c82d8d5dcb854e834bdd83d0d58d750cbbeb23
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-03-26 12:35:27 +0530

    error fixed

--- FirstStep/Services/EmailSevices/IEmailService.cs
+++ FirstStep/Services/EmailSevices/IEmailService.cs
@@ -6,7 +6,7 @@ namespace FirstStep.Services.EmailSevices
     {
         void SendEmail(EmailDto request);
         void OTP(EmailDto request, string reciever, string email, string message);//for signup, company registraion and person verification to allow pasword changin in company portal
-        void SendEmailCompanyRegistration(string email, int type, string company_name, string applicationEvaluationStatusLink);//email for company regisrtation process
+        void SendEmailCompanyRegistration(string email, string company_name, string applicationEvaluationStatusLink);//email for company regisrtation process
         void JobApplicationSuccessfullySentEmail(EmailDto request, string email, string jobseekerFName, string companyName,string jobAdvertisementTitle,string jobApplicationEvaluationStatusLink);//used in seeker portal
         void EvaluatedCompanyRegistraionApplicationEmail(EmailDto request, string email, bool HasAccepted, string comment, string link, string company_name);
     }
