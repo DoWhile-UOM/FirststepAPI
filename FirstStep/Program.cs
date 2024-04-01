8e98585cad1c25da271484b7b32035e1a100db93
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-04-01 15:13:30 +0530

    Update Program.cs

--- FirstStep/Program.cs
+++ FirstStep/Program.cs
@@ -11,13 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //CORS
+/*
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyPolicy", builder => builder.WithOrigins("http://localhost:4200", "https://localhost:4200")
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials());
-});
+});*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -51,8 +52,6 @@ builder.Services.AddSwaggerGen(opt =>
         }
     });
 });
-
-
 
 builder.Services.AddAutoMapper(typeof(Program));
 
