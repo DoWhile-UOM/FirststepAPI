807af5182ccde9a3fa6c7d1e81c98d2c18bdfe7d
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-04-17 13:07:01 +0530

    database server was changed

--- FirstStep/Models/DTOs/ViewCompanyListDto.cs
+++ FirstStep/Models/DTOs/ViewCompanyListDto.cs
@@ -5,7 +5,7 @@
         public int company_id { get; set; }
         public required string company_name { get; set; }
         public required bool verification_status { get; set; }
-        
+        public required int verified_system_admin_id { get; set; }
 
     }
 }
