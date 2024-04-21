7225e3f3fd719d07de9b24f7fe131335a8a47bca
RuwandieD <dharmapalard.21@uom.lk>
2024-04-21 19:18:50 +0530

    a lil  modify

--- FirstStep/Models/DTOs/AddSeekerDto.cs
+++ FirstStep/Models/DTOs/AddSeekerDto.cs
@@ -2,30 +2,21 @@
 {
     public class AddSeekerDto
     {
-        public  string email { get; set; }
-
-        public  string password_hash { get; set; }
-
         public  string first_name { get; set; }
 
         public  string last_name { get; set; }
 
         public int phone_number { get; set; }
 
-        public  string bio { get; set; }
-
-        public  string description { get; set; }
+        public string email { get; set; }
 
         public string? university { get; set; }
 
-        public  string cVurl { get; set; }
-
-        public string? profile_picture { get; set; }
-
         public string? linkedin { get; set; }
 
-        public  int field_id { get; set; }
+        public string bio { get; set; }
 
-        public virtual List<string>? seekerSkills { get; set; }
+        public  string description { get; set; }
+
     }
 }
