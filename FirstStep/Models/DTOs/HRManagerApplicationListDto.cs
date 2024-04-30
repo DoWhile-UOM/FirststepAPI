41d5777aafa12468de4f55bce3afeaa3a8e57607
RuwandieD <dharmapalard.21@uom.lk>
2024-05-01 01:01:01 +0530

    a comment

--- FirstStep/Models/DTOs/HRManagerApplicationListDto.cs
+++ FirstStep/Models/DTOs/HRManagerApplicationListDto.cs
@@ -6,10 +6,11 @@
 
         public string seekerName { get; set; }
 
-        public string status { get; set; }
+        public string status { get; set; } // Yes, No, Pending
 
         public bool is_evaluated { get; set; }
 
+        //hr assistant name
         public DateTime submitted_date { get; set; }
     }
 }
