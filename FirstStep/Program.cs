db0910dd195a2693023be823656d8dcefff5072e
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-05-28 00:08:51 +0530

    Update Program.cs

--- FirstStep/Program.cs
+++ FirstStep/Program.cs
@@ -97,11 +97,15 @@ app.UseCors(options =>
 }); 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
