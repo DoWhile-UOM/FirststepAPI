4166326ee6b66ac9b6db2b09b33f3fa103cac9c1
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-05-28 07:51:28 +0530

    Revert "Update Program.cs"
    This reverts commit db0910dd195a2693023be823656d8dcefff5072e.

--- FirstStep/Program.cs
+++ FirstStep/Program.cs
@@ -97,15 +97,11 @@ app.UseCors(options =>
 }); 
 
 // Configure the HTTP request pipeline.
-
-/*if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}*/
-
-app.UseSwagger();
-app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
