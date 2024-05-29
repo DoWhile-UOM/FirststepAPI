31040b33a4fe50a7164b029b0b313e33e36096f3
RuwandieD <dharmapalard.21@uom.lk>
2024-05-29 20:22:30 +0530

    Application View Dto updated

--- FirstStep/Models/DTOs/ApplicationViewDto.cs
+++ FirstStep/Models/DTOs/ApplicationViewDto.cs
@@ -10,7 +10,25 @@ namespace FirstStep.Models.DTOs
         public int application_Id { get; set; }
 
         public DateTime submitted_date { get; set; }
-        
+
+        public required string email { get; set; }
+
+        public required string first_name { get; set; }
+
+        public required string last_name { get; set; }
+
+        public int phone_number { get; set; }
+
+        public string? bio { get; set; }
+
+        public required string cVurl { get; set; }
+
+        public string? profile_picture { get; set; }
+
+        public string? linkedin { get; set; }
+
+        public IEnumerable<RevisionDto>? revisionList { get; set; }
+
 
 
 
