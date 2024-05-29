719aa852ebed8e1b69b9b472bbdc0965604d67f4
RuwandieD <dharmapalard.21@uom.lk>
2024-05-29 20:00:47 +0530

    revision dto created

---
+++ FirstStep/Models/DTOs/RevisionDto.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstStep.Models.DTOs
+{
+    public class RevisionDto
+    {
+        public int revision_id { get; set; }
+
+        public string? comment { get; set; }
+
+        public required string status { get; set; }
+
+        public DateTime created_date { get; set; }
+
+        public DateTime? last_modified_date { get; set; }
+
+        public int? application_id { get; set; }
+
+        public required int employee_id { get; set; }
+
+    }
+}
