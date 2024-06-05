de46a9086a33ef9860f2bdb69aefc09d8bb43466
RuwandieD <dharmapalard.21@uom.lk>
2024-06-05 17:58:35 +0530

    Revert "some updates"
    This reverts commit 5a0db9f758db95e11f35ee8596ea38c1b729c6f8.

--- FirstStep/Services/RevisionServices/RevisionService.cs
+++ FirstStep/Services/RevisionServices/RevisionService.cs
@@ -43,55 +43,23 @@ namespace FirstStep.Services
 
         public async Task<string> GetCurrentStatus(int applicationID)
         {
-            // Get all revisions for the application
-            var revisions = await _context.Revisions
-                .Include(r => r.employee)
+            Revision? last_revision = await _context.Revisions
                 .Where(r => r.application_id == applicationID)
                 .OrderByDescending(r => r.date)
-                .ToListAsync();
+                .FirstOrDefaultAsync();
 
-            if (!revisions.Any())
+            // need to check whether the latest revision is added by a HR manager or HR assistant
+            // when it added by HR manager, the status is valid
+            // when it added by HR assistant, need to check again any hr manager is added a revision before that
+
+
+            if (last_revision is null)
             {
                 return ApplicationStatus.NotEvaluated.ToString();
             }
 
-            // Get the last revision
-            var last_revision = revisions.FirstOrDefault();
-
-            // Check if the latest revision was added by an HR Manager (HRM) or Company Admin (CA)
-            if (last_revision != null && last_revision.employee != null)
-            {
-                var userType = last_revision.employee.user_type;
-                if (userType == "HRM" || userType == "CA")
-                {
-                    return last_revision.status; // Valid status if added by HR Manager or Company Admin
-                }
-            }
-
-            return last_revision?.status ?? ApplicationStatus.NotEvaluated.ToString();
+            return last_revision.status;
         }
-
-
-
-        //public async Task<string> GetCurrentStatus(int applicationID)
-        //{
-        //    Revision? last_revision = await _context.Revisions
-        //        .Where(r => r.application_id == applicationID)
-        //        .OrderByDescending(r => r.date)
-        //        .FirstOrDefaultAsync();
-
-        //    // need to check whether the latest revision is added by a HR manager or HR assistant
-        //    // when it added by HR manager, the status is valid
-        //    // when it added by HR assistant, need to check again any hr manager is added a revision before that
-
-
-        //    if (last_revision is null)
-        //    {
-        //        return ApplicationStatus.NotEvaluated.ToString();
-        //    }
-
-        //    return last_revision.status;
-        //}
 
         public string GetCurrentStatus(Application application)
         {
@@ -113,25 +81,6 @@ namespace FirstStep.Services
 
         public async Task Create(Revision revision)
         {
-            // Get the last revision for the application
-            var revisions = await _context.Revisions
-                .Where(r => r.application_id == revision.application_id)
-                .OrderByDescending(r => r.date)
-                .ToListAsync();
-
-            // Check if the last revision was added by HRM or CA
-            var last_revision = revisions.FirstOrDefault();
-            if (last_revision != null)
-            {
-                if (last_revision.employee.user_type == "HRM" || last_revision.employee.user_type == "CA")
-                {
-                    if (revision.employee.user_type == "HRA")
-                    {
-                        throw new InvalidOperationException("HRA cannot evaluate this application as it was already evaluated by an HRM or CA.");
-                    }
-                }
-            }
-
             revision.revision_id = 0;
 
             _context.Revisions.Add(revision);
