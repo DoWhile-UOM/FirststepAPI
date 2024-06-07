33d0b68df00325b13cdb8719d5389b2e88b2b1c4
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-06-07 13:34:02 +0530

    change file service to upload files with inline feature

--- FirstStep/Services/FileServices/FileService.cs
+++ FirstStep/Services/FileServices/FileService.cs
@@ -29,6 +29,14 @@ namespace FirstStep.Services
                     file.CopyTo(memoryStream);
                     memoryStream.Position = 0;
                     var client = await _blobcontainerClient.UploadBlobAsync(fileName, memoryStream, default);
+                    //Set Content-Disposition to inline if it's a PDF
+                    if (file.ContentType == "application/pdf")
+                    {
+                        var blobClient = _blobcontainerClient.GetBlobClient(fileName);
+                        var blobHttpHeader = new BlobHttpHeaders();
+                        blobHttpHeader.ContentDisposition = "inline";
+                        await blobClient.SetHttpHeadersAsync(blobHttpHeader);
+                    }
                     azureResponse.Add(client);
                 }
             };
@@ -47,7 +55,13 @@ namespace FirstStep.Services
             {
                await  blobClient.UploadAsync(stream, true);
             }
-
+            //Set Content-Disposition to inline if it's a PDF
+            if (file.ContentType == "application/pdf")
+            {
+                var blobHttpHeader = new BlobHttpHeaders();
+                blobHttpHeader.ContentDisposition = "inline";
+                await blobClient.SetHttpHeadersAsync(blobHttpHeader);
+            }
             return fileName;
         }
 
