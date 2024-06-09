88e8673381e65508950b97b9826b9cb688ab5280
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-06-09 18:38:43 +0530

    Added testcases for CICD

--- FirstStep/Program.cs
+++ FirstStep/Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddAutoMapper(typeof(Program));
 // DataContext Configuration
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("FITiotServerConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection"));
 });
 
 // Azure Blob Storage Configuration
