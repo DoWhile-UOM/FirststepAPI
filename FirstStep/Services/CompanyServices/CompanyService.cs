66e9d85343c4be42fe20c91872003442e4fa24ca
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-06-10 22:05:47 +0530

    feat(company application)- service changes to view pdf

--- FirstStep/Services/CompanyServices/CompanyService.cs
+++ FirstStep/Services/CompanyServices/CompanyService.cs
@@ -106,6 +106,14 @@ namespace FirstStep.Services
         {
             Company company = await FindByID(companyID);
             CompanyApplicationDto companydto = _mapper.Map<CompanyApplicationDto>(company);
+            if(companydto.business_reg_certificate != null)
+            {
+                companydto.business_reg_certificate = await _fileService.GetBlobImageUrl(companydto.business_reg_certificate);
+            }
+            if (companydto.certificate_of_incorporation != null) 
+            {
+                companydto.certificate_of_incorporation = await _fileService.GetBlobImageUrl(companydto.certificate_of_incorporation);
+            }
             return companydto;
         }
         
