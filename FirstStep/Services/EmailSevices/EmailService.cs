0e7f5014097918fc6969b7f438629fb6f866c727
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-06-14 18:09:29 +0530

    Update EmailService.cs

--- FirstStep/Services/EmailSevices/EmailService.cs
+++ FirstStep/Services/EmailSevices/EmailService.cs
@@ -60,7 +60,7 @@ namespace FirstStep.Services
         public async Task SendEmailCompanyRegistration(string email,string company_name, string applicationEvaluationStatusLink)
         {
             // Registration Email
-            applicationEvaluationStatusLink= "http://localhost:4200/RegCheck?id="+applicationEvaluationStatusLink;// this link will direct company to a page where the company can see its regirataion application evaluation status.
+            applicationEvaluationStatusLink= " https://polite-forest-041105700.5.azurestaticapps.net/RegCheck?id=" + applicationEvaluationStatusLink;// this link will direct company to a page where the company can see its regirataion application evaluation status.
                 
             EmailDto request = new();
             var builder = new BodyBuilder();
