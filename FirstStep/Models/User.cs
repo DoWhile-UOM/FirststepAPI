c71f28492a1edc37a37d825a7f07c76311530d6d
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-06-15 21:19:22 +0530

    Get advertisement created user's name in the application list

--- FirstStep/Models/User.cs
+++ FirstStep/Models/User.cs
@@ -16,7 +16,7 @@ namespace FirstStep.Models
 
         public required string last_name { get; set; }
 
-        public required string user_type { get; set; } //User role (Seeker ,SA ,HRM ,HRA, CA )
+        public required string user_type { get; set; }
 
         public string? token { get; set; }
        
