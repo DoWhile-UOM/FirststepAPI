5ce889f1345f662afe303ad9f6ed922bf9c85784
RuwandieD <dharmapalard.21@uom.lk>
2024-07-09 01:53:05 +0530

    Daily sch fixed

--- FirstStep/Controllers/AppointmentController.cs
+++ FirstStep/Controllers/AppointmentController.cs
@@ -49,9 +49,10 @@ namespace FirstStep.Controllers
         [Route("GetSchedulesByDateAndCompany/{date}/Company/{companyId}")]
         public async Task<ActionResult<List<dailyInterviewDto>>> GetSchedulesByDate(DateTime date, int companyId)
         {
-            var schedules = await _appointmentService.GetSchedulesByDate(date,companyId);
+            var schedules = await _appointmentService.GetSchedulesByDate(date, companyId);
             return Ok(schedules);
         }
+
 
         [HttpGet]
         [Route("GetInterviewStat")]
